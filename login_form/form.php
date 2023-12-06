d0d236fdbe7ad8b19b2d23e180ccc9ac5f1bdf1d
username <mail@mail.com>
2023-12-07 00:47:11 +0300

    new file

---
+++ login_form/form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Login Form</title>
+</head>
+<body>
+    <form method="post" action="#">
+        <h2>Login Form</h2>
+        <!-- <input type="email" name="email" placeholder="Enter your email" required> -->
+        <input type="text" name="login" placeholder="Enter your login" required>
+        <input type="password" name="password" placeholder="Enter your password" required>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
+
+
